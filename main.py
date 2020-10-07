9f9c147433aa66b722bf077d7460a6a76bc5cc42
Nicholas Gibb <nicholas.gibb@canada.ca>
2020-10-07 21:29:38 +0000

    Rename fields and apply timezone info

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from dateutil import parser
+from dateutil import parser, tz
+from datetime import datetime, timezone
 import logging
 import sys
 import pandas as pd
@@ -6,56 +7,69 @@ import pandas as pd
 logging.basicConfig(level=logging.DEBUG,
                     format=' %(asctime)s - %(levelname)s - %(message)s')
 
+
 def rename_fields(df):
     """Rename fields to correspond to databases."""
-    df.rename(columns={'Jurisdiction': 'jurisdiction', '# Patients Tested': 'num_pat_tested',
-                       '# Confirmed Positive': 'num_confirmed_pos',
-                       '# Confirmed Negative': 'num_confirmed_neg',
-                       'Change in # Patients Tested': 'change_pat_tested',
-                       'Change in # Confirmed Positive': 'change_confirmed_pos',
-                       'Change in # Confirmed Negative': 'change_confirmed_neg',
-                       'Jurisdictional and Canadian % Positivity Rates': 'jurisdictional_canada_pos_rt',
-                       'Patients Tested per 10^{0} Canadians': 'tests_per_capita_canada',
-                       'Patients Tested per 10^{0} by Jurisdiction': 'tests_per_capita_jurisdiction',
-                       'Date Last Updated': 'update_date'
+    df.rename(columns={'Jurisdiction': 'PT', '# Patients Tested': 'PatientTestedCount',
+                       '# Confirmed Positive': 'ConfirmedPositiveCount',
+                       '# Confirmed Negative': 'ConfirmedNegativeCount',
+                       'Change in # Patients Tested': 'PatientTestedCountChange',
+                       'Change in # Confirmed Positive': 'ConfirmedPositiveCountChange',
+                       'Change in # Confirmed Negative': 'ConfirmedNegativeCountChange',
+                       'Jurisdictional and Canadian % Positivity Rates': 'PTAndCanadianPositivityRatePrct',
+                       'Patients Tested per 10^{0} Canadians': 'CanadianPatientsTestedPerMil',
+                       'Patients Tested per 10^{0} by Jurisdiction': 'PTPatientsTestedPerMil',
+                       'Date Last Updated': 'DateUpdated'
                        }, inplace=True)
-    return df
-
-
-def truncate(df):
-    """Remove the extra rows that are not needed."""
-    df = df[df['jurisdiction'].isin(
-        ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT', 'Total'])]
     return df
 
 
 def jurisdiction(df):
     """Remove asterisk from Ontario."""
-    df['jurisdiction'] = df['jurisdiction'].replace('ON*', 'ON')
+    df['PT'] = df['PT'].replace('ON*', 'ON')
     return df
+
+
+def date_convert(date_to_convert):
+    return datetime.strptime(date_to_convert, '%Y-%m-%d (%H:%M:%S)').replace(tzinfo=tz.timezone('Canada/Central'))
 
 
 def update_date(df):
     """Reformat date to more standard format."""
-    df['update_date'] = df['update_date'].str.replace(
-        '(', '').str.replace(')', '').apply(lambda x: parser.parse(x))
+    # df['DateUpdated'] = df['DateUpdated'].apply(date_convert)
+    tzinfos = {"CST": tz.gettz('Canada/Central')}
+    df['DateUpdated'] = df['DateUpdated'].str.replace(
+        '(', '').str.replace(')', ' CST').apply(lambda x: parser.parse(x, tzinfos=tzinfos))
     return df
 
-def phac_date(df):
+
+def date_loaded(df):
     """Add a new column recording when PHAC received the record."""
-    df['phac_date'] = pd.datetime.now()
+    df['DateLoaded'] = datetime.now().replace(tzinfo=timezone.utc)
     return df
+
 
 def main(data_file):
     logging.info(f'Loading file and transforming fields...')
-    df = pd.read_csv(data_file)
+    dtypes = {
+        'Jurisdiction': 'str',
+        '# Patients Tested': 'int64',
+        '# Confirmed Positive': 'int64',
+        '# Confirmed Negative': 'int64',
+        'Change in # Patients Tested': 'int64',
+        'Change in # Confirmed Positive': 'int64',
+        'Change in # Confirmed Negative': 'int64',
+        'Jurisdictional and Canadian % Positivity Rates': 'float64',
+        'Patients Tested per 10^{0} Canadians': 'float64',
+        'Patients Tested per 10^{0} by Jurisdiction': 'float64'
+    }
+    df = pd.read_csv(data_file, dtype=dtypes, skipfooter=4, engine='python')
     df = rename_fields(df)
     df = jurisdiction(df)
-    df = truncate(df)
     df = update_date(df)
-    df = phac_date(df)
+
+    df = date_loaded(df)
     logging.info(f'Transform complete. Exporting to csv...')
-    print(df)
     df.to_csv(data_file.replace(".csv", "_transformed.csv"), index=False)
 
 
